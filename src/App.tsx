e473f4af3ca03eaf4a673e24b8f254fbed75b23d
lita-alita <mrslitik@gmail.com>
2022-09-22 00:47:37 +0300

    added mvp function

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
+import SenderEth from './components/ethSender';
+
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [isProv, setisProv] = useState(false);
+
+  useEffect(()=>{
+    if (typeof window.ethereum !== 'undefined'){setisProv(true)}
+  }, [])
+
+  //connect Metamask to dApp
+  const connectMetamask = async () => {  
+    const {ethereum} = window;
+    try {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        setCurrentAccount(accounts[0])
+        const id = await ethereum.request({ method: `eth_chainId` });
+        if (id!=='0x4') await changeNetwork();
+    } catch (err) {
+        alert('error to connect')
+    }
+  };
+
+  async function changeNetwork(){
+    const {ethereum} = window;
+    try {
+      await ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: '0x4' }],
+      });
+    } catch (err:any) {
+      // This error code indicates that the chain has not been added to MetaMask.
+      if (err.code === 4902) {
+        try {
+          await ethereum.request({
+            method: 'wallet_addEthereumChain',
+            params: [
+              {
+                chainId: '0x4',
+                chainName: 'Rinkeby testnet',
+                rpcUrls: ['https://rinkeby.infura.io/v3/'],
+              },
+            ],
+          });
+        } catch (err) {
+          alert("Unknown error")
+        }
+      }
+      // handle other "switch" errors
+    }
+  };
+
+  if (!isProv) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          You need to install Metamask
+        </header>
+      </div>
+    )}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    {currentAccount == "" &&
+     <button onClick={connectMetamask}>Click to connect Metamask</button>
+    }
+    {currentAccount !== "" &&
+    <SenderEth/>
+    }
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/ethSender.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import { ethers } from "ethers"
+
+export default function SenderEth () {
+    const [ethAmount, updEthAmount] = useState(0);
+    const [ethAddress, updEthAddress] = useState("");
+
+    async function senderHandler(){
+        const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+        const signer = provider.getSigner();
+        const address = await signer.getAddress()
+        const amount = ethers.utils.parseEther(ethAmount.toString());
+
+        const tx = {
+            from: address,
+            to: ethAddress,
+            value: amount,
+            nonce: provider.getTransactionCount(address, "latest"),
+            gasLimit: ethers.utils.hexlify("0x100000"), // 100000
+            gasPrice: provider.getGasPrice(),
+        };
+
+        const sendedTx = await signer.sendTransaction(tx);
+        console.log(sendedTx)
+        const response = document.getElementById("returner");
+        response!.innerText = `Transaction sended. Hash: ${sendedTx.hash}`
+    }
+    return (
+        <>
+            <input
+                type="number"
+                placeholder="Eth amount"
+                id="amountInput"
+                required
+                onChange={(e => updEthAmount(Number(e.target.value)))}>
+            </input>
+            <input
+                type="text"
+                id="addressInput"
+                placeholder="Eth address"
+                required
+                onChange={(e => updEthAddress(e.target.value))}>
+            </input>
+            <button onClick={senderHandler}>Send</button>
+            <p id="returner"></p>
+        </>
+    )
+}
